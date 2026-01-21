9b568f2b249bf6d75809bd2f74e904d83227ec94
Indra Prasetia <iprasetia60@gmail.com>
2026-01-21 11:35:00 +0800

    gaji

--- app/Http/Controllers/Sdm/GajiUserController.php
+++ app/Http/Controllers/Sdm/GajiUserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Sdm;
 use App\Http\Controllers\Controller;
 use App\Models\Sdm\Gaji;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class GajiUserController extends Controller
 {
@@ -20,22 +20,24 @@ class GajiUserController extends Controller
         return view('sdm.gaji_user.gaji_user', $data);
     }
 
-    //     public function views()
-    // {
-    //     $query = Gaji::query();
-    //     $query->where('nomor_pekerja', 'like', '%' . Auth::user()->nomor_pekerja . '%')
-    //           ->orderBy('bulan', 'desc')
-    //           ->first();
-    //     $data = [];
-    //     foreach ($query->get() as $value) {
-    //         $data[] = [
-    //             'id' => $value->id,
-    //             'nomor_pekerja' => $value->nomor_pekerja,
-    //             'bulan' => Carbon::parse($value->bulan)->format('M Y'),
-    //             'file' => $value->file
-    //         ];
-    //     }
+    public function views()
+    {
+        $nomorPekerja = Session::get('nomor_pekerja');
 
-    //     return response()->json($data, 200);
-    // }
+        $query = Gaji::where('nomor_pekerja', $nomorPekerja)
+            ->orderBy('bulan', 'desc')
+            ->get();
+
+        $data = [];
+        foreach ($query as $value) {
+            $data[] = [
+                'id' => $value->id,
+                'nomor_pekerja' => $value->nomor_pekerja,
+                'bulan' => Carbon::parse($value->bulan)->format('M Y'),
+                'file' => $value->file
+            ];
+        }
+
+        return response()->json($data, 200);
+    }
 }
