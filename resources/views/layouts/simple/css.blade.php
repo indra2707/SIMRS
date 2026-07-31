1bfcce6f64aed1bf97bb820b1f8de0ecd21cb652
Indra Prasetia <iprasetia60@gmail.com>
2026-07-31 14:51:36 +0800

    url pbm

--- resources/views/layouts/simple/css.blade.php
+++ resources/views/layouts/simple/css.blade.php
@@ -1,35 +1,56 @@
-    @vite(['public/assets/scss/app.scss'])
-    {{-- ICONS --}}
-    <!-- Font Awesome-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.css')}}">
-    <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/icofont.css')}}">
-    <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/themify.css')}}">
-    <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/flag-icon.css')}}">
-    <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/feather-icon.css')}}">
-    <!-- Plugins css start-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/animate.css') }}">
+@vite(['public/assets/scss/app.scss'])
+{{-- ICONS --}}
+<!-- Font Awesome-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.css')}}">
+<!-- ico-font-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/icofont.css')}}">
+<!-- Themify icon-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/themify.css')}}">
+<!-- Flag icon-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/flag-icon.css')}}">
+<!-- Feather icon-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/feather-icon.css')}}">
+<!-- Plugins css start-->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/animate.css') }}">
 
-    {{-- Select2 --}}
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/select2.css')}}">
+{{-- Select2 --}}
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/select2.css')}}">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+<link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
-    {{-- Bootstrap Table --}}
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap-table/dist/bootstrap-table.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap-table/dist/extensions/fixed-columns/bootstrap-table-fixed-columns.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/sweetalert2.css')}}">
+{{-- Bootstrap Table --}}
+<link rel="stylesheet" href="{{ asset('assets/bootstrap-table/dist/bootstrap-table.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('assets/bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('assets/bootstrap-table/dist/extensions/fixed-columns/bootstrap-table-fixed-columns.min.css') }}">
+<link rel="stylesheet"
+    href="{{ asset('assets/bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/sweetalert2.css')}}">
 
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/vendors/toastr.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/vendors/toastr.min.css')}}">
 
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/date-picker.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/date-picker.css')}}">
 
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/jquery.signature.package-1.2.1/css/jquery.signature.css')}}">
-    {{-- <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css"> --}}
-    {{-- <link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css" rel="stylesheet">  --}}
+<link rel="stylesheet" type="text/css"
+    href="{{asset('assets/vendor/jquery.signature.package-1.2.1/css/jquery.signature.css')}}">
+{{--
+<link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css"> --}}
+{{--
+<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css"
+    rel="stylesheet"> --}}
+
+<!-- url pbm -->
+<script type="text/javascript">
+(function(w,d,s,r,k,h,m){
+	if(w.performance && w.performance.timing && w.performance.navigation) {
+		w[r] = w[r] || function(){(w[r].q = w[r].q || []).push(arguments)};
+		h=d.createElement('script');h.async=true;h.setAttribute('src',s+k);
+		d.getElementsByTagName('head')[0].appendChild(h);
+		(m = window.onerror),(window.onerror = function (b, c, d, f, g) {
+		m && m(b, c, d, f, g),g || (g = new Error(b)),(w[r].q = w[r].q || []).push(["captureException",g]);})
+	}
+})(window,document,'https://rum.ihc.id/RUMScript.js?appKey=','apmr','oSos2qZmbdj3Nca1Y2H8nOhbkF5EuBBg150005420');
+</script>
